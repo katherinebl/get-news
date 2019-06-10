5d474b53ed1ace367f7453bf0d9e25e9874b567a
Katherine Briceño <kath.gbl@gmail.com>
2019-06-10 21:59:16 +0200

    Added fetch news funtionality

---
+++ index.js
@@ -0,0 +1,18 @@
+import { topHeadlinesUrl } from "./newsApi.js";
+
+window.addEventListener("load", () => {
+  getNews();
+});
+
+async function getNews() {
+  const results = await fetch(topHeadlinesUrl);
+  const json = await results.json();
+
+  const main = document.querySelector("main");
+
+  json.articles.forEach(article => {
+    const el = document.createElement("news-article");
+    el.article = article;
+    main.appendChild(el);
+  });
+}
---
+++ newsApi.js
@@ -0,0 +1,5 @@
+const apiKey = "ef2d9768ed764f84b3d209a6cc9f6fbc";
+const topHeadlinesUrl =
+  "https://newsapi.org/v2/top-headlines?country=us&apiKey=" + apiKey;
+
+export { apiKey, topHeadlinesUrl };
